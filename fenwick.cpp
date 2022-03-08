ac652dc45d9d1786f3f5e66cad9525b765789428
fonmagnus <arnold777armageddon@gmail.com>
2022-03-08 09:02:56 +0700

    add BIT

---
+++ fenwick.cpp
@@ -0,0 +1,25 @@
+# include <bits/stdc++.h>
+using namespace std;
+# define int long long
+
+const int MAXN = 1e5;
+int bit[MAXN+5];
+int ls(int x) { return x & (-x); }
+
+void upd(int x, int val){
+  for(int i=x; i<=MAXN; i+=ls(i)) bit[i] += val;
+}
+
+int get(int x){
+  int res = 0;
+  for(int i=x; i>0; i-=ls(i)) res += bit[i];
+  return res;
+}
+
+int get(int l, int r){
+  return get(r) - get(l-1);
+}
+
+signed main(){
+  ios_base :: sync_with_stdio(false);
+}
