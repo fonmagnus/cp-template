e84606dc2b101e7f6f8b59198fabce5c8008e1d6
fonmagnus <arnold777armageddon@gmail.com>
2022-03-09 08:03:55 +0700

    add gitignore and modify template

--- template.cpp
+++ template.cpp
@@ -3,23 +3,18 @@ using namespace std;
 typedef long long ll;
 
 # define int long long
-
-# define rep(i, n) for(int i=0; i<n; i++)
-# define repi(i, n) for(int i=1; i<=n; i++)
-# define each(x, v) for(auto x : v)
-
-# define Int(a) int a; cin >> a;
-# define String(a) string a; cin >> a;
-# define Double(a) double a; cin >> a;
-# define Arr(a, n) vector<int> a(n); for(int i=0; i<n; i++) cin >> a[i];
+# define For(i, n) for(int i=0; i<n; i++)
+# define Fori(i, n) for(int i=1; i<=n; i++)
+# define Each(x, v) for(auto x : v)
 
 int solve(){
+
   return 0;
 }
 
 signed main(){
   ios_base :: sync_with_stdio(false);
-  Int(t);
+  int t; cin >> t;
   while(t--){
     int ans = solve();
   }
