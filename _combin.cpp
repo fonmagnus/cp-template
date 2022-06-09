ca7649ace8c00ee22d667d1a1f5640dd7e67a49f
fonmagnus <arnold777armageddon@gmail.com>
2022-06-09 14:14:23 +0700

    modify combin

--- _combin.cpp
+++ _combin.cpp
@@ -1,25 +1,25 @@
 # include <bits/stdc++.h>
 using namespace std;
 # define int long long
-const int MOD = 1e9 + 7;
-const int MAXN = 2e5;
+const int mod = 1e9 + 7;
+const int maxn = 2e5;
 
-int add(int a, int b, int mod = MOD) { return (a+b) % mod; }
-int sub(int a, int b, int mod = MOD) { return (a-b+mod) % mod; }
-int mul(int a, int b, int mod = MOD) { return (a*b) % mod; }
-int pw(int a, int b, int mod = MOD){
+int add(int a, int b) { return (a+b) % mod; }
+int sub(int a, int b) { return (a-b+mod) % mod; }
+int mul(int a, int b) { return (a*b) % mod; }
+int pw(int a, int b){
   if(b == 0) return 1;
-  int t = pw(a, b/2, mod);
-  if(b%2 == 0) return mul(t, t, mod);
-  return mul(a, mul(t,t,mod), mod);
+  int t = pw(a, b/2);
+  if(b%2 == 0) return mul(t, t);
+  return mul(a, mul(t,t));
 }
 
-int f[MAXN+5], fi[MAXN+5];
+int f[maxn+5], fi[maxn+5];
 void precompute(){
   f[0] = 1;
-  for(int i=1; i<=MAXN; i++) f[i] = mul(f[i-1], i);
-  fi[MAXN] = pw(f[MAXN], MOD-2);
-  for(int i=MAXN-1; i>=0; i--) fi[i] = mul(fi[i+1], i+1);
+  for(int i=1; i<=maxn; i++) f[i] = mul(f[i-1], i);
+  fi[maxn] = pw(f[maxn], mod-2);
+  for(int i=maxn-1; i>=0; i--) fi[i] = mul(fi[i+1], i+1);
 }
 int C(int n, int k) { return n < k ? 0 : mul(f[n], mul(fi[n-k], fi[k])); }
 
