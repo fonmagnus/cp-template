9accc018377b8e73888389e9fa0c2e48ce243b92
fonmagnus <arnold777armageddon@gmail.com>
2022-06-09 20:37:58 +0700

    add fenwick and modnum

--- _fenwick.cpp
+++ _fenwick.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 # define int long long
 
-const int MAXN = 1e5;
-int bit[MAXN+5];
+const int maxn = 1e5;
+int bit[maxn+5];
 int ls(int x) { return x & (-x); }
 
 void upd(int x, int val){
-  for(int i=x; i<=MAXN; i+=ls(i)) bit[i] += val;
+  for(int i=x; i<=maxn; i+=ls(i)) bit[i] += val;
 }
 
 int get(int x){
--- _modnum.cpp
+++ _modnum.cpp
@@ -1,17 +1,17 @@
 # include <bits/stdc++.h>
 using namespace std;
 # define int long long
-const int MOD = 1e9 + 7;
-const int MAXN = 2e5;
+const int mod = 1e9 + 7;
+const int maxn = 2e5;
 
-int add(int a, int b, int mod = MOD) { return (a+b) % mod; }
-int sub(int a, int b, int mod = MOD) { return (a-b+mod) % mod; }
-int mul(int a, int b, int mod = MOD) { return (a*b) % mod; }
-int pw(int a, int b, int mod = MOD){
+int add(int a, int b) { return (a+b) % mod; }
+int sub(int a, int b) { return (a-b+mod) % mod; }
+int mul(int a, int b) { return (a*b) % mod; }
+int pw(int a, int b){
   if(b == 0) return 1;
-  int t = pw(a, b/2, mod);
-  if(b%2 == 0) return mul(t, t, mod);
-  return mul(a, mul(t,t,mod), mod);
+  int t = pw(a, b/2);
+  if(b%2 == 0) return mul(t, t);
+  return mul(a, mul(t,t));
 }
 
 signed main(){
